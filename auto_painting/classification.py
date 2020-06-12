0c803524d3af102ea9165997a31015e9c24a3f0b
soyoung <64260525+psy1825@users.noreply.github.com>
2020-06-12 18:20:36 +0900

    Update classification.py

--- auto_painting/classification.py
+++ auto_painting/classification.py
@@ -46,10 +46,9 @@ class classification():
             elif pre_ans == 1: pre_ans_str = "체리"
             elif pre_ans == 2: pre_ans_str = "토마토"
             elif pre_ans == 3: pre_ans_str = "꽃"
-            elif pre_ans == 4: pre_ans_str = "아보카도"
-            elif pre_ans == 5: pre_ans_str = "나뭇잎"
-            elif pre_ans == 6: pre_ans_str = "당근"
-            elif pre_ans == 7: pre_ans_str = "조개"
+            elif pre_ans == 4: pre_ans_str = "나뭇잎"
+            elif pre_ans == 5: pre_ans_str = "당근"
+            elif pre_ans == 6: pre_ans_str = "조개"
 
             if i[0] >= 0.8:
                 print("해당 " + filenames[cnt].split("\\")[1] + "이미지는 " + pre_ans_str + "으로 추정됩니다.")
@@ -65,14 +64,11 @@ class classification():
                 self.label = 'flower'
             elif i[4] >= 0.8:
                 print("해당 " + filenames[cnt].split("\\")[1] + "이미지는 " + pre_ans_str + "으로 추정됩니다.")
-                self.label = 'avocado'
+                self.label = 'leaf'
             elif i[5] >= 0.8:
                 print("해당 " + filenames[cnt].split("\\")[1] + "이미지는 " + pre_ans_str + "으로 추정됩니다.")
-                self.label = 'leaf'
-            elif i[6] >= 0.8:
-                print("해당 " + filenames[cnt].split("\\")[1] + "이미지는 " + pre_ans_str + "으로 추정됩니다.")
                 self.label = 'carrot'
-            elif i[7] >= 0.8:
+            elif i[6] >= 0.8:
                 print("해당 " + filenames[cnt].split("\\")[1] + "이미지는 " + pre_ans_str + "으로 추정됩니다.")
                 self.label = 'shellfish'
             else:
