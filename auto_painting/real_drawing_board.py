f9bac3410f8b3cd9c175ed2a38e34a7ff5b21e15
yujeong0 <48202271+yujeong0@users.noreply.github.com>
2020-06-12 12:47:20 +0900

    update drawing board

--- auto_painting/real_drawing_board.py
+++ auto_painting/real_drawing_board.py
@@ -15,18 +15,62 @@ class drawing_board(QWidget):
         QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)  # 화면크기스케일링
         self.file = ''
 
-        # 전체 폼 박스
-        formbox = QHBoxLayout()
-        self.setLayout(formbox)
+        self.formbox = QVBoxLayout()
 
-        # 좌, 우 레이아웃박스
-        left = QVBoxLayout()
-        right = QVBoxLayout()
+        self.formbox1 = QHBoxLayout()
+
+        self.left = QVBoxLayout()
+        self.right = QVBoxLayout()
         self.right2 = QVBoxLayout()
+        self.bottom = QHBoxLayout()
 
-        # 그룹박스1 생성 및 좌 레이아웃 배치
+        self.groupBox1()        # 그룹박스1
+        self.groupBox2()        # 그룹박스2
+        self.groupBox3()        # 그룹박스3
+        self.groupBox4()        # 그룹박스4
+
+        # painting 버튼
+        paintingbutton = QPushButton('자동 채색', self)
+        self.left.addWidget(paintingbutton)
+        paintingbutton.clicked.connect(self.load_image)
+
+        self.left.addStretch(1)  # 그냥 레이아웃 여백 추가
+
+        # 우 레이아웃 박스에 그래픽 뷰 추가
+        self.view = CView(self)
+        self.view.setFixedWidth(300)
+        self.view.setFixedHeight(300)
+        self.right.addWidget(self.view)
+
+        # 제일 오른쪽 레이아웃에 빈 흰색 배경
+        pixmap = QPixmap('whiteimage.png')
+        self.lbl_img = QLabel()
+        self.lbl_img.setPixmap(pixmap)
+        self.right2.addWidget(self.lbl_img)
+
+        # 폼박스1에 레이아웃 박스 배치
+        self.formbox1.addLayout(self.left)
+        self.formbox1.addLayout(self.right)
+        self.formbox1.addLayout(self.right2)
+
+        self.formbox1.setStretchFactor(self.left, 0)
+        self.formbox1.setStretchFactor(self.right, 1)
+
+        self.formbox2 = QHBoxLayout()
+        self.label = QLabel('왼쪽 박스에 그림을 그려주세요', self)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.formbox2.addWidget(self.label)
+
+        self.formbox.addLayout(self.formbox1)
+        self.formbox.addLayout(self.formbox2)
+
+        self.setLayout(self.formbox)
+        self.setGeometry(100, 100, 700, 400)
+
+
+    def groupBox1(self):
         gb = QGroupBox('그리기 종류')
-        left.addWidget(gb)
+        self.left.addWidget(gb)
 
         # 그룹박스1 에서 사용할 레이아웃
         box = QVBoxLayout()
@@ -44,9 +88,9 @@ class drawing_board(QWidget):
         self.radiobtns[1].setChecked(True)
         self.drawType = 1
 
-        # 그룹박스2
+    def groupBox2(self):
         gb = QGroupBox('펜 설정')
-        left.addWidget(gb)
+        self.left.addWidget(gb)
 
         grid = QGridLayout()
         gb.setLayout(grid)
@@ -69,9 +113,9 @@ class drawing_board(QWidget):
         self.penbtn.clicked.connect(self.showColorDlg)
         grid.addWidget(self.penbtn, 1, 1)
 
-        # 그룹박스3
+    def groupBox3(self):
         gb = QGroupBox('붓 설정')
-        left.addWidget(gb)
+        self.left.addWidget(gb)
 
         hbox = QHBoxLayout()
         gb.setLayout(hbox)
@@ -85,55 +129,21 @@ class drawing_board(QWidget):
         self.brushbtn.clicked.connect(self.showColorDlg)
         hbox.addWidget(self.brushbtn)
 
-        # 그룹박스4
+    def groupBox4(self):
         gb = QGroupBox('지우개')
-        left.addWidget(gb)
+        self.left.addWidget(gb)
 
-        hbox = QHBoxLayout()
-        gb.setLayout(hbox)
+        vbox = QVBoxLayout()
+        gb.setLayout(vbox)
 
         self.checkbox = QCheckBox('지우개 동작')
         self.checkbox.stateChanged.connect(self.checkClicked)
-        hbox.addWidget(self.checkbox)
+        vbox.addWidget(self.checkbox)
 
         # 전체 지우기
         removebutton = QPushButton('전체 지우기', self)
-        left.addWidget(removebutton)
+        vbox.addWidget(removebutton)
         removebutton.clicked.connect(self.remove_all)
-
-        # 사진저장 버튼
-        # savebutton = QPushButton('그림 저장', self)
-        # left.addWidget(savebutton)
-        # savebutton.clicked.connect(self.save_image)
-
-        # painting 버튼
-        paintingbutton = QPushButton('자동 채색', self)
-        left.addWidget(paintingbutton)
-        paintingbutton.clicked.connect(self.load_image)
-
-        left.addStretch(1)  # 그냥 레이아웃 여백 추가
-
-        # 우 레이아웃 박스에 그래픽 뷰 추가
-        self.view = CView(self)
-        self.view.setFixedWidth(300)
-        self.view.setFixedHeight(300)
-        right.addWidget(self.view)
-
-        # 제일 오른쪽 레이아웃에 빈 흰색 배경
-        pixmap = QPixmap('whiteimage.png')
-        self.lbl_img = QLabel()
-        self.lbl_img.setPixmap(pixmap)
-        self.right2.addWidget(self.lbl_img)
-
-        # 전체 폼박스에 레이아웃 박스 배치
-        formbox.addLayout(left)
-        formbox.addLayout(right)
-        formbox.addLayout(self.right2)
-
-        formbox.setStretchFactor(left, 0)
-        formbox.setStretchFactor(right, 1)
-
-        self.setGeometry(100, 100, 700, 400)
 
     def radioClicked(self):
         for i in range(len(self.radiobtns)):
@@ -159,16 +169,6 @@ class drawing_board(QWidget):
             self.brushcolor = color
             self.brushbtn.setStyleSheet('background-color: {}'.format(color.name()))
 
-    def save_image(self):
-        date = datetime.now()
-        filename = 'Screenshot ' + date.strftime('%Y-%m-%d_%H-%M-%S.png')
-        img = QPixmap(self.view.grab(self.view.sceneRect().toRect()))
-        self.file = "./multi_img_data/imgs_others_test_sketch/" + filename
-        img.save(self.file, 'png')
-        img = cv2.imread(self.file, 0)
-        img = img[2:476, 2:663]     #사진 저장할 때 끝부분 모서리 지우기
-        cv2.imwrite(self.file, img)
-
     def remove_all(self):
         for i in self.view.scene.items():
             self.view.scene.removeItem(i)
@@ -181,16 +181,32 @@ class drawing_board(QWidget):
 
         reply = self.msg_box('\"'+ label +'\"을 그린 게 맞다면 \"OK\" 버튼을, 다시 그리고 싶다면 \"NO\" 버튼을 눌러주세요.')
         if reply == True:
+            self.label.setText('채색중... 잠시만 기다려주세요!')
+            self.label.repaint()
+
             self.lbl_img.hide()  # 전 이미지 숨김
             fill = Fill_color(self.file, label)    #이미지 색칠
             self.file = fill.file
-            # print(self.file)
 
             pixmap = QPixmap(self.file)  # jpg 는 안되는데 왜 안되는 지 아직 모르겠다..
 
             self.lbl_img = QLabel()
             self.lbl_img.setPixmap(pixmap)
             self.right2.addWidget(self.lbl_img)
+
+            self.label.setText('채색이 완료되었습니다!')
+            self.label.repaint()
+
+    def save_image(self):
+        date = datetime.now()
+        filename = 'Screenshot ' + date.strftime('%Y-%m-%d_%H-%M-%S.png')
+        img = QPixmap(self.view.grab(self.view.sceneRect().toRect()))
+        self.file = "./multi_img_data/imgs_others_test_sketch/" + filename
+        img.save(self.file, 'png')
+        img = cv2.imread(self.file, 0)
+        img = img[2:476, 2:663]     #사진 저장할 때 끝부분 모서리 지우기
+        cv2.imwrite(self.file, img)
+
 
     def msg_box(self, text):
         msgBox = QMessageBox()
